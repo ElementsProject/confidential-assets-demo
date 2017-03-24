b04d4ef8514bcc94a6daab0a3147f473766edc03
nakagawa <takatoshi@dglab.com>
2017-03-24 13:22:45 -0700

    Dave: Added doc file.

---
+++ src/dave/doc.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 DG Lab
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/*
+Dave runs a coffee shop. His customers may either pay him in cash or they can use
+a specific asset that he supports.
+*/
+package main
