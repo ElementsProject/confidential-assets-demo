6b4871dc370a10b3f4a8150870ce75f6344aaf85
Akio Nakamura <akio-nakamura@MN16112801.local>
2017-03-24 13:22:15 -0700

    Charlie: Added doc file.

---
+++ src/charlie/doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2017 DG Lab
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/*
+Charlie is a Point Exchange service. He is willing to exchange points between
+different asset types. He does this by looking at a conversion table, composing
+transactions in response to requests to convert points.
+*/
+package main
