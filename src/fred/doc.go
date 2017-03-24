a9d7d942eed4b5e56975d527fed235efc0eaf8c8
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-03-24 13:23:47 -0700

    Fred: Added doc file.

---
+++ src/fred/doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2017 DG Lab
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/*
+Fred is a "block signer". He is responsible for creating and putting blocks
+on the block chain at appropriate times (when the mempool has something in it).
+In reality, he would be one of several functionaries in the block chain.
+*/
+package main
