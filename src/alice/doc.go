289c502d22ccaa6921c637d8c564da6758cf21cb
Akio Nakamura <akio-nakamura@MN16112801.local>
2017-03-24 13:19:42 -0700

    Alice: Added doc.go file.

---
+++ src/alice/doc.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 DG Lab
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/*
+Alice is the customer in the demo.
+
+She wants to buy coffee from Dave, but since she doesn't have the asset he accepts
+she uses a Point Exchange service (Charlie) to convert her existing points to the
+ones Dave wants.
+*/
+package main
