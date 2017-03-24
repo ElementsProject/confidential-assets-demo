0612c68b32630b44e982e2156f9c28f299e48aae
nakagawa <takatoshi@dglab.com>
2017-03-24 13:20:51 -0700

    Bob: Added doc.go file.

---
+++ src/bob/doc.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 DG Lab
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/*
+Bob is a competitor of Dave's.
+
+He wants to find out as much as he can about what Dave is doing, so he can strategize
+how to take Dave out of business for good. He is actively snooping on the block chain
+and logs everything he can find about all transactions.
+*/
+package main
